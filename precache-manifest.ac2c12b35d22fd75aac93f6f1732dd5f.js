ab7e5cfa1d659b73f2c3d73d82edea59ec2a1c4b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-25 22:19:36 +0000

    Deploy christopherkade/twitter-thread-generator to github.com/christopherkade/twitter-thread-generator.git:gh-pages

---
+++ precache-manifest.ac2c12b35d22fd75aac93f6f1732dd5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4570eb86f2753971be70774c3aa34e99",
+    "url": "/twitter-thread-generator/index.html"
+  },
+  {
+    "revision": "2f123d6ae6b6f2677159",
+    "url": "/twitter-thread-generator/static/css/main.1bb75b1b.chunk.css"
+  },
+  {
+    "revision": "8488ba8ddc6bbd9c9098",
+    "url": "/twitter-thread-generator/static/js/2.3f716869.chunk.js"
+  },
+  {
+    "revision": "2f123d6ae6b6f2677159",
+    "url": "/twitter-thread-generator/static/js/main.41b6c4ab.chunk.js"
+  },
+  {
+    "revision": "efc49d812c8141bbac74",
+    "url": "/twitter-thread-generator/static/js/runtime~main.0ac42fbc.js"
+  },
+  {
+    "revision": "6ca35672729549d6258943d626c04a13",
+    "url": "/twitter-thread-generator/static/media/twitter.6ca35672.svg"
+  }
+]);
